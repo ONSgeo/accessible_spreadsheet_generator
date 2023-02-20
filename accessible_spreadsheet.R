7bc7063422b329c5bcc07fbe1f6161079c468b20
Calder <helen.calder@ons.gov.uk>
2023-02-20 17:41:13 +0000

    Exploring using openxlsx to format the output xlsx

--- accessible_spreadsheet.R
+++ accessible_spreadsheet.R
@@ -1,9 +1,7 @@
-#packages
-install.packages("writexl")
-
 library(tidyverse)
 library(readxl)
 library(writexl)
+library(openxlsx)
 
 #get some data :)
 
@@ -146,6 +144,43 @@ LAD_filter <- filter(LAD_lu, RGN22CD == test_rgn) %>%
 
 #celebrate/cry if it doesn't work
 
+raw_head <- head(raw_data)
+
+#Using openxlsx to create and format a new xlsx workbook
+wb <- createWorkbook() #create the workbook
+modifyBaseFont(wb, fontSize = 14, fontColour = 'black', fontName = "Arial") #define the font, size & colour
+
+#add sheets to the workbook
+addWorksheet(wb, sheetName = "Cover Sheet", gridLines = TRUE)
+addWorksheet(wb, sheetName = "Accessible Data", gridLines = TRUE)
+addWorksheet(wb, sheetName = "Tidy Data", gridLines = TRUE)
+
+#write data to sheet 1
+writeData(wb, 1, "Listing geographical areas in tables - best practice examples", startCol = "A", startRow = 1)
+writeData(wb, 1, "This spreadsheet contains two worksheets. Each includes a table that lists geographical areas.", startCol = "A", startRow = 2)
+writeData(wb, 1, "One shows an accessible layout which meets the accessibility legislation that came into force in September 2020.", startCol = "A", startRow = 3)
+writeData(wb, 1, "The other shows a 'tidy data' layout which is useful for machine readability. ", startCol = "A", startRow = 4)
+
+
+#write data to sheet 2
+writeData(wb, 2, "Layout of geographical areas - accessible version", startCol = "A", startRow = 1)
+writeData(wb, 2, "This worksheet contains one table. The table contains some blank cells due to the layout of the geographical areas.", startCol = "A", startRow = 2)
+writeDataTable(wb, sheet = 2, raw_head, startCol = "A", startRow = 3)
+#TODO figure out how to deal with accented chars in Welsh names - UTF8 ? error - 
+#"Error in stri_length(newStrs) : 
+#invalid UTF-8 byte sequence detected; try calling stri_enc_toutf8()
+
+
+#write the workbook
+saveWorkbook(wb, "test_output_formatting.xlsx", overwrite = TRUE)
+
+
+
+
+
+
+
+####Notes####
 #functions
 add_one<- function(x){
   y <- x + 1
