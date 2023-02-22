1f0d91739bdfef8d8f9ab6ac89cb467dac6166e9
Calder <helen.calder@ons.gov.uk>
2023-02-22 14:53:21 +0000

    Starting to build code for data layout

--- accessible_spreadsheet.R
+++ accessible_spreadsheet.R
@@ -10,12 +10,27 @@ raw_head <- head(raw_data)
 
 ####Preparing data layout####
 
-
 #create a new col in raw_data with the first 3 char of the entity code in it
+ent_data <- raw_data %>% mutate(ENTCD = substr(AREACD, 1, 3))
+
 #do unique on that col to get a list of all the entity codes that are used in our data
+distinct_entities <- ent_data %>% distinct(ENTCD, .keep_all = FALSE)
+#or use this in base R to create a vector
+unique_entities <- unique(ent_data$ENTCD)
+
+#TODO check the unique_entities %in% is assessing all entities against the defined vector
 #then use an if statement to load in the appropriate lookup based on what hierarchy the entity code falls in
+if(unique_entities %in% c("K02","K03", "K04", "E92", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "E13", "W92", "W06", "S92", "S12", "N92", "N09") == TRUE){
+  lookup <- read_csv("lookups/CTRY22_NAT22_RGN22_CTYUA22_LAD22_lookup.csv") #admin hierarchy lookup
+} else {
+  message("Lookup not found")
+}
+
+input_data_col_name <- "AREACD"
 #join the raw_data to the appropriate lookup
-#QA checks to make sure everything has joined correctly
+ent_data_join <- left_join(lookup, ent_data, by = c("AREA22CD" = input_data_col_name))
+#QA checks to make sure everything has joined correctly - number of rows, what entities have NA values and which cols
+na_data <- filter(ent_data_join, is.na(Value) == TRUE)
 #prep the data for export
 # :)
 
